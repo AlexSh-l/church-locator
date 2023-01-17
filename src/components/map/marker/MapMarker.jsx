ada442eeff8f30914363492c7f55287b25a045ae
AlexSh-l <ashust75@gmail.com>
2023-01-18 00:20:39 +0300

    Prevent clickthrough marker

--- src/components/map/marker/MapMarker.jsx
+++ src/components/map/marker/MapMarker.jsx
@@ -4,7 +4,8 @@ import { MAP_MARKER } from "../../../constants/CssClasses";
 
 const MapMarker = (props) => {
     return (
-        <div className={MAP_MARKER} lat={props.lat} lng={props.lng} onClick={() => {
+        <div className={MAP_MARKER} lat={props.lat} lng={props.lng} onClick={(e) => {
+            e.stopPropagation();
             (props.popupInfo && (props.info.id === props.popupInfo.id))
                 ? props.setPopupInfo(null)
                 : props.setPopupInfo({ ...props.info })
